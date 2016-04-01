c79fa62b3fd60eb4c2bc9700e8187483a21bb3f2
Arkhypov Yeregrin <Arkhypov Yeregrin>
2016-04-01 16:49:26 -0400

    commit for other enviroment to invoke browser

---
+++ SeleniumIntermediate/src/us/st/selenium/extension/WebElementExtender.java
@@ -0,0 +1,44 @@
+package us.st.selenium.extension;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.internal.WrapsDriver;
+
+public class WebElementExtender {
+
+	private static WebDriver driver;
+	
+	public static void setAttribute(WebElement element, String attributeName, String value){
+		WrapsDriver wrappedElement = (WrapsDriver) element;
+		JavascriptExecutor driver = (JavascriptExecutor) wrappedElement.getWrappedDriver();
+		driver.executeScript("arguments[0].setAttribute(arguments[1],arguments[2])", element, attributeName, value);
+	}
+	
+	private static void highlightElement(WebElement element) throws InterruptedException{
+		for (int i=0; i<5; i++){
+		Thread.sleep(1000);
+		WrapsDriver wrappedElement = (WrapsDriver) element;
+		JavascriptExecutor driver = (JavascriptExecutor) wrappedElement.getWrappedDriver();
+		driver.executeScript("arguments[0].setAttribute('style', arguments[1])", 
+			element, "color: green; border:10px; solid yellow");
+		driver.executeScript("arguments[0].setAttribute('style', arguments[1]);", element, "color: green; border:10px; solid yellow" );
+		}
+	}
+	
+	@Test
+	public void test() {
+		
+		// invoke driver, go to url
+		WebElement username = driver.findElement(By.id("username"));
+		WebElement email = driver.findElement(By.id("email"));
+		setAttribute(username, "value", "");
+		username.sendKeys("testuser.com");
+		
+	}
+
+}

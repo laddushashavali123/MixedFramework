e3367633e3223e814bd046b5b55235ca9b69f7e4
Arkhypov Yeregrin <Arkhypov Yeregrin>
2016-06-06 17:28:30 -0400

    removed extra files

--- SeleniumIntermediate/src/us/st/selenium/tasksTest.java
+++ SeleniumIntermediate/src/us/st/selenium/tasksTest.java
@@ -1,6 +1,6 @@
 package us.st.selenium;
 
-import static org.junit.Assert.*;
+
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,10 +16,12 @@ public class tasksTest {
 
 	@Test
 	public void test() {
+		int[] B = {1,2,5,7,2,3,5};
 		//function #1
-		System.out.println(parseString("work #@#$ bike bannana fun, perl, ##!()**ruby, function"));
+		//System.out.println(parseString("work #@#$ bike bannana fun, perl, ##!()**ruby, function"));
 		//function #2 - return unique values
-		System.out.println(parseUniqueValues("perl,python,ruby,java,go,erlang,perl,python,java,java"));
+		//System.out.println(parseUniqueValues("perl,python,ruby,java,go,erlang,perl,python,java,java"));
+		System.out.println(Solution.solution(5, B));
 	}
 	
 	//function #1 first implementation
@@ -81,12 +83,99 @@ public class tasksTest {
 	 * 	api/users/1 - [{"user_id":1, "user_name":"Luis"},{"user_id":2, "user_name":"mike"}]
 	 */
 	
+	// function #4 - write function to get max value in array
+	
+	public int getMax(List <Integer> arr){
+		int result= arr.get(0);
+		for (int i=0; i<arr.size();i++){
+			if (arr.get(i)>result){
+				result=arr.get(i);
+			}
+		}
+		
+		return result;
+		
+	}
+
+	
+/*	
+	class Solution {
+	    public int solution(int[] A) {
+	        int result= -1;
+	        List<Integer> indexes = new ArrayList<Integer>();
+	        int tmpl= 0;
+	        int tmpr= 0;
+	        
+	        if (!((A.length==1)&&(A.length==0))){
+	        	//for (int P=0; P<=A.length-1; P++)
+	            for (int i=0; i<A.length-1; i++){
+	            	for(int j=0; j<i; j++){
+	                tmpl=+A[j];
+	                tmpr=+A[A.length-1-j];
+	                }
+	            tmpl=+A[i];
+	            
+	            if (tmpl==tmpr){
+	            	indexes.add(i);
+	            }
+	            }
+	            
+	            int rnd = new Random().nextInt(indexes.size());
+	            result = indexes.get(rnd);
+	            }
+	        return result;
+	    }
+*/
+	}
+	class Solution {
+		// function #5 - split array in the right way
+		public static int solution(int X, int[] A) {
+	        int K = 0;
+	        int amount = 0;
+	        //int[] B = {5,5,1,7,2,3,5};
+	        if (!((A.length==1))){
+	        	
+	            for (int i=0; i<A.length; i++){
+	            	
+	            	if (A[i]==X){
+	            		for (int j=i+1; j<A.length;j++){
+	            		if(!(A[j]==X)){
+	            			amount = j-i;
+	            			for (int k=A.length-1; k>=i+amount; k--){
+		            			if (A[k]==X){
+		    	            		continue;
+		            		}
+		            			for (int e=k; e>=i+amount;e--){
+		    	            		if(A[e]==X){
+		    	            			break;
+		    	            		}
+		    	            		if(k-e==amount){
+		    	            		System.out.println("Solution found!");
+		    	            		K = e+1;
+		    	            		}
+		    	            		}
+		            			break;
+		            		}
+	            			break;
+	            		}
+	            		}
+	            		break;
+	            		
+	            		
+	            		}
+	            	}
+	        }
+	        return K;
+		}
+	    }
+	            	
+
 	
 		
 
 	
 	
 	
-}
+
 	
 

b0b0a107e56bed97daeef558a6fd4da06a25b4c8
yevgen.arkhypov <y.arkhypov@gmail.com>
2017-06-16 13:54:41 -0400

    configuration commit

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 git tag | xargs git tag -d
 git filter-branch --index-filter "git rm --cached --ignore-unmatch $FILE"
 rm -rf .git/refs/original/ .git/refs/remotes/ .git/*_HEAD .git/logs/
-git for-each-ref --format="%(403e932b2a6395d47ea060505d3241be2b594fca)" refs/original/ | \
+git for-each-ref --format="403e932b2a6395d47ea060505d3241be2b594fca" refs/original/ | \
   xargs -n1 --no-run-if-empty git update-ref -d
   git reflog expire --expire-unreachable=now --all
   git repack -A -d
